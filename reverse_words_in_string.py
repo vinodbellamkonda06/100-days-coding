034949e3949748fd2ba8aba710a5f5885f981b7a
unknown <bkvinod477@gmail.com>
2024-02-20 22:28:37 +0530

    day3 reversing the words in a string

---
+++ reverse_words_in_string.py
@@ -0,0 +1,22 @@
+
+## method1
+#def reverse_words_in_string(s):
+    #return " ".join(s.split()[::-1])
+#print(reverse_words_in_string("hello vinod"))
+
+## method2
+
+def reverse_words_in_string(s):
+    temp = ""
+    words = []
+    for char in s:
+        if char != " ":
+            temp += char
+        else:
+            words.append(temp)
+            temp = ""
+    words.append(temp)
+    return " ".join(words[::-1])
+print(reverse_words_in_string("hello vinod"))  
+        
+ 
