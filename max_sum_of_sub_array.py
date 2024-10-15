17e6da097027872c4fb2a5d966523cd8a3a37330
unknown <bkvinod477@gmail.com>
2024-10-15 16:26:04 -0500

    modifying

--- max_sum_of_sub_array.py
+++ max_sum_of_sub_array.py
@@ -1,8 +1,13 @@
 def max_sum_of_sub_arrays(array,size):
+    """
+
+    :param array:  List of integers
+    :param size:  int
+    :return: sub array List of integers
+    """
     sub_array = []
     for i in range(len(array)):
         sub_array.append(array[i:i + size])
         
     return max([array for array in sub_array if len(array) == size])
-     
-print(max_sum_of_sub_arrays([2, 3, 4, 5, 6, 7],3))
+
--- reverse_words_in_string.py
+++ reverse_words_in_string.py
@@ -1,10 +1,10 @@
 
 ## method1
-#def reverse_words_in_string(s):
-    #return " ".join(s.split()[::-1])
-#print(reverse_words_in_string("hello vinod"))
+def reverse_words_in_string(s):
+    return " ".join(s.split()[::-1])
+print(reverse_words_in_string("hello vinod"))
 
-## method2
+## method2: with out using builtins
 
 def reverse_words_in_string(s):
     temp = ""
@@ -17,6 +17,6 @@ def reverse_words_in_string(s):
             temp = ""
     words.append(temp)
     return " ".join(words[::-1])
-print(reverse_words_in_string("hello vinod"))  
+
         
  
